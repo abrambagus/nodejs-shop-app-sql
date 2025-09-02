f4aeb7ea98f073209a58f22bc3d250ad008f39f5
abram <abrambagus@gmail.com>
2025-09-02 10:42:41 +0700

    inserting data to database

--- models/product.js
+++ models/product.js
@@ -1,7 +1,5 @@
 const db = require("../util/database");
 
-const Cart = require("./cart");
-
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
     this.id = id;
@@ -11,7 +9,12 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {}
+  save() {
+    return db.execute(
+      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
+      [this.title, this.price, this.imageUrl, this.description]
+    );
+  }
 
   static deleteById(id) {}
 
