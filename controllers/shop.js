7260d91eed813761ff16a74f4afd4649ce9e5483
abram <abrambagus@gmail.com>
2025-09-04 15:13:18 +0700

    fetchng orders & reset cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -87,9 +87,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -106,16 +108,25 @@ exports.postOrder = (req, res, next) => {
         .catch((err) => console.log(err));
     })
     .then(() => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(() => {
       res.redirect("/orders");
     })
     .catch((err) => console.log(err));
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    pageTitle: "Checkout",
-    path: "/checkout",
-  });
+exports.getOrders = (req, res, next) => {
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
